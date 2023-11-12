94bbe3d871d65e35092b9a2662cc9fb9900c2733
ErFosi <alvarodiezandino@gmail.com>
2023-11-12 20:48:25 +0100

    Pagina V1.0

--- web/app.js
+++ web/app.js
@@ -1,59 +1,120 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const path = require('path');
 
-const db = mysql.createConnection({
+const dbConfig = {
     host: 'mysql', // El nombre del contenedor MySQL en la red Docker
     user: 'web_admin', // Usuario MySQL
     password: 'admin_sistemas', // Contraseña MySQL
-    database: 'mensajes' // Nombre de la base de datos
-});
+    database: 'mensajes', // Nombre de la base de datos
+    port: 3306
+};
 
 let loggedInUser = null; // Variable global para almacenar el nombre de usuario
+let db;
 
-db.connect((err) => {
-    if (err) {
-        console.error('Error al conectar a la base de datos:', err);
-    } else {
-        console.log('Conexión a la base de datos exitosa');
-    }
-});
+const connectWithRetry = () => {
+    db = mysql.createConnection(dbConfig);
+
+    db.connect((err) => {
+        if (err) {
+            console.error('Error al conectar a la base de datos:', err);
+            console.log('Reintentando la conexión en 5 segundos...');
+            setTimeout(connectWithRetry, 5000); // Reintentar después de 5 segundos
+        } else {
+            console.log('Conexión a la base de datos exitosa');
+            const sql = 'INSERT INTO usuarios (usuario, contrasena) VALUES (?, ?)';
+            db.query(sql, ["test", "test"], (err, result) => {});
+        }
+    });
+};
+
+// Iniciar la conexión con reintento
+connectWithRetry();
+
+app.use(session({
+    secret: 'tu_secreto', // Cambia esto a una cadena segura
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 60 * 1000 } // 1 hora en milisegundos
+}));
 
 app.use(express.json());
-
+app.use('/public', express.static('public'));
+app.use(express.static('public'));
 // Ruta para autenticar usuario
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    const sql = 'SELECT * FROM usuarios WHERE username = ? AND password = ?';
+    const sql = 'SELECT * FROM usuarios WHERE usuario = ?';
 
-    db.query(sql, [username, password], (err, rows) => {
+    db.query(sql, [username], async (err, rows) => {
         if (err) {
-            console.error('Error al autenticar usuario:', err);
-            res.json({ loggedIn: false });
-        } else {
-            if (rows.length > 0) {
-                loggedInUser = username; // Almacenar el nombre de usuario
-                res.json({ loggedIn: true });
-            } else {
-                res.json({ loggedIn: false });
+            console.error('Error al buscar usuario en la base de datos:', err);
+            return res.json({ loggedIn: false, message: 'Error al autenticar usuario' });
+        }
+        console.log("usuario")
+        console.log(username)
+        console.log(password)
+        console.log(rows)
+        if (rows.length > 0) {
+            const storedPassword = rows[0].contrasena;
+            console.log(rows)
+            // Verificar la contraseña hasheada
+            try {
+                const match = await bcrypt.compare(password, storedPassword);
+                if (match) {
+                    req.session.loggedInUser = username;
+                    res.json({ loggedIn: true, message: 'Credenciales correctas' });
+                    return;
+                } else {
+                    res.json({ loggedIn: false, message: 'Credenciales incorrectas' });
+                }
+            } catch (error) {
+                console.error('Error al comparar contraseñas:', error);
+                res.json({ loggedIn: false, message: 'Error al autenticar usuario' });
             }
+        } else {
+            res.json({ loggedIn: false, message: 'Usuario no encontrado' });
         }
     });
 });
 
-// Ruta para registrar un nuevo usuario
-app.post('/register', (req, res) => {
-    const { username, password } = req.body;
-    const sql = 'INSERT INTO usuarios (username, password) VALUES (?, ?)';
 
-    db.query(sql, [username, password], (err, result) => {
-        if (err) {
-            console.error('Error al registrar usuario:', err);
-            res.json({ registered: false });
-        } else {
-            res.json({ registered: true });
-        }
-    });
+
+
+
+
+
+
+// Ruta para registrar un nuevo usuario
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+
+    // Verificar que la contraseña tenga al menos 6 caracteres
+    if (password.length < 6) {
+        return res.json({ registered: false, message: 'La contraseña debe tener al menos 6 caracteres' });
+    }
+
+    // Generar el hash de la contraseña
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const sql = 'INSERT INTO usuarios (usuario, contrasena) VALUES (?, ?)';
+        db.query(sql, [username, hashedPassword], (err, result) => {
+            if (err) {
+                console.error('Error al registrar usuario:', err);
+                res.json({ registered: false });
+            } else {
+                res.json({ registered: true });
+            }
+        });
+    } catch (error) {
+        console.error('Error al hashear la contraseña:', error);
+        res.json({ registered: false });
+    }
 });
 
 // Ruta para obtener mensajes
@@ -71,17 +132,21 @@ app.get('/messages', (req, res) => {
 // Ruta para enviar mensajes
 app.post('/messages', (req, res) => {
     const { mensaje } = req.body;
+    const loggedInUser = req.session.loggedInUser;
+
+    if (!loggedInUser) {
+        return res.status(401).json({ sent: false, message: 'Usuario no autenticado' });
+    }
 
     db.query('INSERT INTO mensajes (autor, mensaje) VALUES (?, ?)', [loggedInUser, mensaje], (err, results) => {
         if (err) {
             console.error('Error al enviar mensaje:', err);
-            res.json({ sent: false });
+            res.status(500).json({ sent: false, message: 'Error interno del servidor' });
         } else {
             res.json({ sent: true });
         }
     });
 });
-
 // Ruta para obtener el usuario loggeado
 app.get('/getLoggedInUser', (req, res) => {
     res.json({ username: loggedInUser });
@@ -94,13 +159,21 @@ app.post('/logout', (req, res) => {
 });
 
 // Definir ruta para la página principal (inicio de sesión / registro)
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/login.html');
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
 });
 // Ruta para el dashboard
 app.get('/dashboard', (req, res) => {
-    res.sendFile(__dirname + '/views/dashboard.html');
+    // Verificar si hay una sesión iniciada
+    if (req.session.loggedInUser) {
+        // Si hay una sesión iniciada, redirigir al dashboard
+        res.sendFile(path.join(__dirname, 'views', 'dashboard.html'));
+    } else {
+        // Si no hay una sesión iniciada, redirigir al inicio de sesión u otra página
+        res.redirect('/login'); // Puedes ajustar esto según tus necesidades
+    }
 });
+
 app.listen(3000, () => {
     console.log('Servidor iniciado en http://localhost:3000');
 });
--- web/public/js/script.js
+++ web/public/js/script.js
@@ -1,3 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Manejar el formulario de mensajes si está presente
+    var messageForm = document.getElementById('messageForm');
+    if (messageForm) {
+        messageForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+            var message = document.getElementById('message').value;
+            sendMessage(message);
+            document.getElementById('message').value = '';
+        });
+    }
+
+    // Manejar el formulario de inicio de sesión si está presente
+    var loginForm = document.getElementById('loginForm');
+    if (loginForm) {
+        loginForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+            var username = document.getElementById('loginUsername').value;
+            var password = document.getElementById('loginPassword').value;
+            handleLogin(username, password);
+        });
+    }
+
+    // Manejar el formulario de registro si está presente
+    var registerForm = document.getElementById('registerForm');
+    if (registerForm) {
+        registerForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+            var newUsername = document.getElementById('registerUsername').value;
+            var newPassword = document.getElementById('registerPassword').value;
+            handleRegister(newUsername, newPassword);
+        });
+    }
+});
+
+
 function sendMessage(message) {
     fetch('/messages', {
         method: 'POST',
@@ -6,62 +42,41 @@ function sendMessage(message) {
         },
         body: JSON.stringify({ mensaje: message })
     })
-    .then(response => response.json())
-    .then(data => {
-        if (data.sent) {
-            console.log('Mensaje enviado correctamente');
-            getMessages();
-        } else {
-            console.error('Error al enviar mensaje');
-        }
-    })
-    .catch(error => {
-        console.error('Error al enviar mensaje:', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            if (data.sent) {
+                console.log('Mensaje enviado correctamente');
+                getMessages();
+            } else {
+                console.error('Error al enviar mensaje');
+            }
+        })
+        .catch(error => {
+            console.error('Error al enviar mensaje:', error);
+        });
 }
 
 function getMessages() {
     fetch('/messages')
-    .then(response => response.json())
-    .then(data => {
-        showMessages(data);
-    })
-    .catch(error => {
-        console.error('Error al obtener mensajes:', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            showMessages(data);
+        })
+        .catch(error => {
+            console.error('Error al obtener mensajes:', error);
+        });
 }
 
 function showMessages(messages) {
     var messagesElement = document.getElementById('messages');
     messagesElement.innerHTML = '';
 
-    messages.forEach(function(message) {
+    messages.forEach(function (message) {
         var messageDiv = document.createElement('div');
         messageDiv.innerHTML = '<strong>' + message.autor + ':</strong> ' + message.mensaje;
         messagesElement.appendChild(messageDiv);
     });
 }
-
-document.getElementById('messageForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var message = document.getElementById('message').value;
-    sendMessage(message);
-    document.getElementById('message').value = '';
-});
-
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    handleLogin(username, password);
-});
-
-document.getElementById('registerForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var newUsername = document.getElementById('newUsername').value;
-    var newPassword = document.getElementById('newPassword').value;
-    handleRegister(newUsername, newPassword);
-});
 
 function handleLogin(username, password) {
     fetch('/login', {
@@ -73,10 +88,11 @@ function handleLogin(username, password) {
     })
     .then(response => response.json())
     .then(data => {
+        console.log('Respuesta del servidor:', data);
         if (data.loggedIn) {
             console.log('Inicio de sesión exitoso');
-            getLoggedInUser();
-            window.location.href = '/dashboard.html'; // Redirigir al usuario a dashboard
+            window.location.href = '/dashboard';
+           
         } else {
             console.error('Error al iniciar sesión');
         }
@@ -94,27 +110,28 @@ function handleRegister(username, password) {
         },
         body: JSON.stringify({ username, password })
     })
-    .then(response => response.json())
-    .then(data => {
-        if (data.registered) {
-            console.log('Registro exitoso');
-            handleLogin(username, password); // Iniciar sesión después de registrarse
-        } else {
-            console.error('Error al registrar');
-        }
-    })
-    .catch(error => {
-        console.error('Error al registrar:', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            console.log('Respuesta del servidor:', data);
+            if (data.registered) {
+                console.log('Registro exitoso');
+                handleLogin(username, password); 
+            } else {
+                console.error('Error al registrar');
+            }
+        })
+        .catch(error => {
+            console.error('Error al registrar:', error);
+        });
 }
 
 function getLoggedInUser() {
     fetch('/getLoggedInUser')
-    .then(response => response.json())
-    .then(data => {
-        console.log('Usuario loggeado:', data.username);
-    })
-    .catch(error => {
-        console.error('Error al obtener usuario loggeado:', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            console.log('Usuario loggeado:', data.username);
+        })
+        .catch(error => {
+            console.error('Error al obtener usuario loggeado:', error);
+        });
 }
